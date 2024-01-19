35f241044bc08c490ad3acc85176fc783ee49f9e
thomais oliveira da silva <thomaisoliveiradasilva@hotmail.com>
2024-01-18 22:18:49 -0300

    mais um desafio cumprido

---
+++ script.js
@@ -0,0 +1,19 @@
+let diadaSemana = prompt('Olá que dia da semana é Hoje?')
+
+if( diadaSemana == 'domingo') {
+    confirm(`Há que bom hoje é ${diadaSemana} BOM FIM DE SEMANA`)
+}else if( diadaSemana == 'segunda'){
+    confirm(`que pena hoje é ${diadaSemana} ESPERO QUE O FIM DE SEMANA CHEQUE LOGO`)
+}else if( diadaSemana == 'terça'){
+    confirm(`que pena hoje é ${diadaSemana} ESPERO QUE O FIM DE SEMANA CHEQUE LOGO`)
+}else if( diadaSemana == 'quarta'){
+    confirm(`que pena hoje é ${diadaSemana} ESPERO QUE O FIM DE SEMANA CHEQUE LOGO`)
+}else if( diadaSemana == 'quinta'){
+    confirm(`que pena hoje é ${diadaSemana} ESPERO QUE O FIM DE SEMANA CHEQUE LOGO`)
+}else if(diadaSemana == 'sexta') {
+    confirm(`Há que bom hoje é ${diadaSemana} Sextou tem CABARE essa noite`)
+}else if(diadaSemana == 'sábado') {
+    confirm(`Há que bom hoje é ${diadaSemana} BOM FIM DE SEMANA`)
+}else {
+    confirm(`você digitou o dia da semana de forma errada ${diadaSemana} NÃO é dia da semana. Por favor apertar OK ou aperte F5`)
+}
